48d7c488142e7f0eaafb9df1e3c78864fdac582e
YuvalArama <112100174+YuvalArama@users.noreply.github.com>
2023-02-18 15:53:51 +0200

    basicStart

---
+++ projectOffical/model.ts
@@ -0,0 +1,58 @@
+class Card{
+    id:number
+    category:string
+    face:string
+    back:string
+    
+    constructor(
+        public cardId:number,
+        public cardCategory:string,
+        public cardFace:string,
+        public cardBack:string
+    ){}
+}
+
+
+const cards:Card[]=[
+    new Card(1,"common","./projectPhotos/1.PNG","./projectPhotos/cardBack.PNG"),
+    new Card(2,"common","./projectPhotos/2.PNG","./projectPhotos/cardBack.PNG"),
+    new Card(3,"common","./projectPhotos/3.PNG","./projectPhotos/cardBack.PNG"),
+    new Card(4,"common","./projectPhotos/4.PNG","./projectPhotos/cardBack.PNG"),
+    new Card(5,"common","./projectPhotos/5.PNG","./projectPhotos/cardBack.PNG"),
+    new Card(6,"common","./projectPhotos/6.PNG","./projectPhotos/cardBack.PNG"),
+    new Card(7,"common","./projectPhotos/7.PNG","./projectPhotos/cardBack.PNG"),
+    new Card(8,"common","./projectPhotos/8.PNG","./projectPhotos/cardBack.PNG"),
+    new Card(9,"common","./projectPhotos/9.PNG","./projectPhotos/cardBack.PNG"),
+    new Card(10,"common","./projectPhotos/10.PNG","./projectPhotos/cardBack.PNG"),
+    new Card(11,"common","./projectPhotos/11.PNG","./projectPhotos/cardBack.PNG"),
+    new Card(12,"common","./projectPhotos/12.PNG","./projectPhotos/cardBack.PNG"),
+    new Card(13,"common","./projectPhotos/13.PNG","./projectPhotos/cardBack.PNG"),
+    new Card(14,"common","./projectPhotos/14.PNG","./projectPhotos/cardBack.PNG"),
+    new Card(15,"common","./projectPhotos/15.PNG","./projectPhotos/cardBack.PNG"),
+    new Card(16,"common","./projectPhotos/16.PNG","./projectPhotos/cardBack.PNG"),
+    new Card(17,"common","./projectPhotos/17.PNG","./projectPhotos/cardBack.PNG"),
+    new Card(18,"common","./projectPhotos/18.PNG","./projectPhotos/cardBack.PNG"),
+    new Card(19,"common","./projectPhotos/19.PNG","./projectPhotos/cardBack.PNG"),
+    new Card(20,"common","./projectPhotos/20.PNG","./projectPhotos/cardBack.PNG"),
+    new Card(21,"common","./projectPhotos/21.PNG","./projectPhotos/cardBack.PNG"),
+    new Card(22,"common","./projectPhotos/22.PNG","./projectPhotos/cardBack.PNG"),
+    new Card(23,"common","./projectPhotos/23.PNG","./projectPhotos/cardBack.PNG"),
+    new Card(24,"common","./projectPhotos/24.PNG","./projectPhotos/cardBack.PNG"),
+    new Card(25,"common","./projectPhotos/25.PNG","./projectPhotos/cardBack.PNG"),
+    new Card(26,"common","./projectPhotos/26.PNG","./projectPhotos/cardBack.PNG"),
+    new Card(27,"common","./projectPhotos/27.PNG","./projectPhotos/cardBack.PNG"),
+    new Card(28,"common","./projectPhotos/28.PNG","./projectPhotos/cardBack.PNG"),
+    new Card(29,"common","./projectPhotos/29.PNG","./projectPhotos/cardBack.PNG"),
+    new Card(30,"common","./projectPhotos/30.PNG","./projectPhotos/cardBack.PNG"),
+]
+
+
+cards.forEach((card)=>{
+    const cardElement = document.createElement("div")
+    cardElement.classList.add("card")
+    cardElement.innerHTML = `
+    <img class = "front" src="${cards[0].cardFace}">
+    <img class = "back" src="${cards[0].cardBack}">
+
+    `
+})
