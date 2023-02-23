14a6f22270779b5e6c52a9decca21540a866b13e
MaximusGitPrime <112101036+MaximusGitPrime@users.noreply.github.com>
2023-02-23 23:01:36 +0200

    Maxim Update controlts

--- projectOffical/helpers.ts
+++ projectOffical/helpers.ts
@@ -2,7 +2,7 @@
 function generateRandomNumbers() {
     console.log("random func activated");
     
-    const numbers:any = [];
+    let numbers:any = [];
     while (numbers.length < 5) {
       const random = Math.floor(Math.random() * (30-0)) + 0;
       if (!numbers.includes(random)) {
