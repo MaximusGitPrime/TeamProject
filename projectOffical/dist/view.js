48391eb63d9f09b4b16c7cc607a3984901658a1d
MaximusGitPrime <112101036+MaximusGitPrime@users.noreply.github.com>
2023-03-09 21:01:54 +0200

    maxim view.ts update

---
+++ projectOffical/dist/view.js
@@ -0,0 +1,13 @@
+var containerCard = document.querySelectorAll(".container__card");
+console.log(containerCard);
+// function showCardsinAlbum(){
+//     const currentUserString = localStorage.getItem('current_user');
+//     const userCardsFromLocalStorage:any = localStorage.getItem(`${currentUserString}.cards`)
+//     const userCards = JSON.parse(userCardsFromLocalStorage);
+//     console.log(userCards);
+//     userCards.forEach(userCard => {
+//         if(userCard.cardId === )
+//         userCard.cardId
+//     });
+// }
+// showCardsinAlbum()
---
+++ projectOffical/view.ts
@@ -0,0 +1,18 @@
+const containerCard = document.querySelectorAll(".container__card") 
+console.log(containerCard);
+
+// function showCardsinAlbum(){
+
+//     const currentUserString = localStorage.getItem('current_user');
+//     const userCardsFromLocalStorage:any = localStorage.getItem(`${currentUserString}.cards`)
+//     const userCards = JSON.parse(userCardsFromLocalStorage);
+//     console.log(userCards);
+//     userCards.forEach(userCard => {
+//         if(userCard.cardId === )
+//         userCard.cardId
+        
+//     });
+
+// }
+
+// showCardsinAlbum()
